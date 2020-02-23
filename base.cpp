8da280c1c3b09e7198e8ffa63b0c49cd2807822d
Aliaksei Rak <kventinel@gmail.com>
2020-02-23 19:03:01 +0300

    update base cpp file

--- base.cpp
+++ base.cpp
@@ -1,12 +1,28 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 #define F first
 #define S second
+#define pb push_back
+#define eb emplace_back
 
 using namespace std;
 
 using ll = long long;
 using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 
 int main() {
     ios_base::sync_with_stdio(false);
